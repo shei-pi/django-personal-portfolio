91822aaf632dea83c4e3614bf93f475007322bf9
shei-pi <jprabino@gmail.com>
2022-06-23 22:46:08 -0300

    ADDING TESGTS

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -1,3 +1,42 @@
+"TDD Demo!"
+from urllib import response
 from django.test import TestCase
 
-# Create your tests here.
+class DemoTests(TestCase):
+    
+    def test_ping_get(self):
+        """GET Ping
+        """
+        response = self.client.get('/ping/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content.decode('utf8'),'pong')
+        
+    def test_ping_head(self):
+        """HEAD ping
+        """
+        response = self.client.head('/ping/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"")
+        self.assertGreater(int(response["Content-Length"]), 0)
+    def test_ping_options(self):
+        """OPTIONS ping
+        """
+        response = self.client.options('/ping/')
+        self.assertEqual(response.status_code, 200) 
+        self.assertEqual(int(response["Content-Length"]), 0)
+        for method in ["GET", "HEAD", "OPTIONS"]:
+            with self.subTest(method=method):
+                self.assertIn(method, response["Allow"], f"{method} not in ALLOW header")
+    
+    def test_ping_method_not_allowed(self):
+        """Do we handle disallowed mehtods
+        """
+        not_allowed = ["post", "put", "patch", "delete", "trace"]
+        for method in not_allowed:
+            with self.subTest(method=method):
+                call_method = getattr(self.client, method)
+                response = call_method('/ping/')
+                self.assertEqual(response.status_code, 405)
+            
+        
+        
